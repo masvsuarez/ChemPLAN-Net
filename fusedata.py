009782ebf88719824cebecbb078a66666bd0fe0d
mike72353 <47592534+mike72353@users.noreply.github.com>
2020-04-29 16:05:57 +0800

    Create fusedata.py

---
+++ fusedata.py
@@ -0,0 +1,49 @@
+'''
+Merges Relevant KB data into one single ALL.SUM data file for input, output and annotations.
+'''
+import pickle
+import numpy as np
+import time
+
+
+env = 'ALI.CT' # ALI.CT, ARG.CZ, ARO.PSEU, CON.PSEU, COO.PSEU, HIS.PSEU, HYD.OH, LYS.NZ, PRO.PSEU, RES.N, RES.O, TRP.NE1
+env2 = 'ARG.CZ'
+env3 = 'ARO.PSEU'
+env4 = 'CON.PSEU'
+env5 = 'COO.PSEU'
+env6 = 'HIS.PSEU'
+env7 = 'HYD.OH'
+env8 = 'LYS.NZ'
+env9 = 'PRO.PSEU'
+env10 = 'RES.N'
+env11 = 'RES.O'
+env12 = 'TRP.NE1'
+envN = 'PRT.SNW'
+
+string1 = 'Homogenised.annotation.txt'
+string2 = 'Homogenised.boundfrags.txt'
+string3 = 'Homogenised.property.pvar'
+
+def filenms(filetype):
+    filenames = ['%s/%s.%s'  %(env, env, filetype), '%s/%s.%s' %(env2, env2, filetype), '%s/%s.%s' %(env3, env3, filetype), '%s/%s.%s' %(env4, env4, filetype), '%s/%s.%s' %(env5, env5, filetype), '%s/%s.%s' %(env6, env6, filetype), '%s/%s.%s' %(env7, env7, filetype), '%s/%s.%s' %(env8, env8, filetype), '%s/%s.%s' %(env9, env9, filetype), '%s/%s.%s' %(env10, env10, filetype), '%s/%s.%s' %(env11, env11, filetype), '%s/%s.%s' %(env12, env12, filetype)]
+    return filenames
+
+trainNew = pickle.load(open("../DataP_New/"+filenms(string3)[0], "rb"))
+print(trainNew.shape)
+for i in filenms(string3)[1:]:
+    train2 = pickle.load(open("../DataP_New/"+i, "rb"))
+    trainNew = np.append(trainNew, train2, axis =0)
+    print(trainNew.shape)
+pickle.dump(trainNew, open("../DataP_New/"+"%s/%s.Homogenised.property.pvar" %(envN, envN), "wb"))
+    
+with open("../DataP_New/"+'%s/%s.Homogenised.annotation.txt'  %(envN, envN), 'w') as outfile:
+    for fname in filenms(string1):
+        with open("../DataP_New/"+fname) as infile:
+            outfile.write(infile.read())
+            
+with open("../DataP_New/"+'%s/%s.Homogenised.boundfrags.txt'  %(envN, envN), 'w') as outfile:
+    for fname in filenms(string2):
+        with open("../DataP_New/"+fname) as infile:
+            outfile.write(infile.read())        
+
+print('bound created')
